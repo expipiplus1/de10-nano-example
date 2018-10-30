d30b96ce3c08ddde3766a5d27c154387e52c2411
Joe Hermaszewski <git@monoid.al>
2018-10-30 13:17:42 +0800

    Everything booting and networking working!

--- configuration.nix
+++ configuration.nix
@@ -53,22 +53,69 @@ in {
   boot.kernelPackages =
     let linux_socfpga = pkgs.callPackage ./linux-socfpga.nix {
           kernelPatches = [
-            { name = "de10-nano";
-              patch = (pkgs.fetchFromGitHub {
-                owner = "intel";
-                repo = "meta-de10-nano";
-                rev = "9ad8a88e1d1181ba6f54c79ecebb81c1d68042ee";
-                sha256 = "1111111111111111111111111111111111111111111111111111111111111111";
-              }) + "/recipes-kernel/linux/config/socfpga-4.1-ltsi/patches/0001-Add-DE10-Nano-devicetree.patch";
+            { name = "de10-nano-devicetree";
+              patch = ./de10-nano-devicetree.patch;
             }
           ];
           defconfig = "socfpga_defconfig";
+          structuredExtraConfig = with lib; {
+            LBDAF = "y";
+
+            USB_GADGET_DEBUG = "y";
+            USB_GADGET_VERBOSE = "y";
+            USB_GADGET_DEBUG_FILES = "y";
+            USB_GADGET_DEBUG_FS = "y";
+            U_SERIAL_CONSOLE = "y";
+            USB_FUSB300 = "y";
+            USB_FOTG210_UDC = "y";
+            USB_GR_UDC = "y";
+            USB_R8A66597 = "y";
+            USB_PXA27X = "y";
+            USB_MV_UDC = "y";
+            USB_MV_U3D = "y";
+            USB_SNP_CORE = "y";
+            USB_SNP_UDC_PLAT = "y";
+            USB_M66592 = "y";
+            USB_BDC_UDC = "y";
+            USB_BDC_PCI = "y";
+            USB_AMD5536UDC = "y";
+            USB_NET2272 = "y";
+            USB_NET2272_DMA = "y";
+            USB_NET2280 = "y";
+            USB_GOKU = "y";
+            USB_EG20T = "y";
+            USB_GADGET_XILINX = "y";
+            USB_DUMMY_HCD = "y";
+            USB_LIBCOMPOSITE = "y";
+            USB_F_ACM = "y";
+            USB_U_SERIAL = "y";
+            USB_U_ETHER = "y";
+            USB_F_OBEX = "y";
+            USB_F_NCM = "y";
+            USB_F_ECM = "y";
+            USB_F_EEM = "y";
+            USB_F_SUBSET = "y";
+            USB_F_RNDIS = "y";
+            USB_F_FS = "y";
+            USB_CONFIGFS = "y";
+            USB_CONFIGFS_ACM = "y";
+            USB_CONFIGFS_OBEX = "y";
+            USB_CONFIGFS_NCM = "y";
+            USB_CONFIGFS_ECM = "y";
+            USB_CONFIGFS_ECM_SUBSET = "y";
+            USB_CONFIGFS_RNDIS = "y";
+            USB_CONFIGFS_EEM = "y";
+            USB_CONFIGFS_F_FS = "y";
+            USB_ETH = "y";
+            USB_ETH_RNDIS = "y";
+            USB_ETH_EEM = "y";
+          };
         };
     in  pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_socfpga);
 
   system.fsPackages = [ pkgs.dosfstools ];
 
-  boot.initrd.kernelModules = [ "vfat" "ip_tables" "nls-cp437" ];
+  boot.initrd.kernelModules = [ "vfat" ];
 
   # Disable some other stuff we don't need.
   security.sudo.enable = false;
@@ -84,6 +131,53 @@ in {
     permitRootLogin = "yes";
   };
 
+  services.dhcpd4 = {
+    enable = true;
+    interfaces = [ "usb0" ];
+    extraConfig = ''
+      option subnet-mask 255.255.255.0;
+      option broadcast-address 192.168.7.255;
+      subnet 192.168.7.0 netmask 255.255.255.0 {
+        range 192.168.7.6 192.168.7.7;
+      }
+    '';
+  };
+
+  systemd.services.rndis = {
+      description = "Bring up RNDIS host";
+      before = [ "network.target" ];
+      wantedBy = [ "network.target" ];
+      after = [ "network-pre.target" ];
+      script = ''
+        cd /sys/kernel/config
+
+        mkdir usb_gadget/g1
+        cd usb_gadget/g1
+
+        echo 0x0525 > idVendor
+        echo 0xa4a2 > idProduct
+        mkdir strings/0x409
+        echo Linux > strings/0x409/manufacturer
+        echo "Etherned/RNDIS gadget" > strings/0x409/product
+
+        mkdir configs/c.1
+        echo 100 > configs/c.1/MaxPower
+        mkdir configs/c.1/strings/0x409
+        echo "RNDIS" > configs/c.1/strings/0x409/configuration
+
+        mkdir functions/rndis.usb0 # use default parameters
+
+        ln -s functions/rndis.usb0 configs/c.1
+
+        echo ffb40000.usb > UDC
+      '';
+  };
+
+
+  networking.interfaces.usb0.ipv4.addresses = [ { address = "192.168.7.1"; prefixLength = 24; } ];
+  networking.dhcpcd.denyInterfaces = [ "usb0" ];
+  networking.defaultGateway = { address = "192.168.7.6"; interface = "usb0"; };
+
   # Tell the Nix evaluator to garbage collect more aggressively.
   # This is desirable in memory-constrained environments that don't
   # (yet) have swap set up.
@@ -98,25 +192,6 @@ in {
 
   # Allow the user to log in as root without a password.
   users.users.root.initialHashedPassword = "";
-
-  # systemd.timers."joe-debug" =
-  #                     {
-  #                       wantedBy = [ "timers.target" ];
-  #                       timerConfig = {
-  #                         OnCalendar = "10 s";
-  #                         Persistent = "yes";
-  #                       };
-  #                     };
-
-  systemd.services."joe-debug" = {
-    description = "debug echo";
-    startAt = "*:*:0/3";
-    script = ''
-      echo hello > /hello
-      sync
-      # systemctl status >> /status
-    '';
-  };
 
   sdImage = {
     populateBootCommands = let
