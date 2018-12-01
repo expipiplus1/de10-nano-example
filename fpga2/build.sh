2fef677936c29c9e565b6b336cbb8ff8b865eb06
Joe Hermaszewski <git@monoid.al>
2018-12-01 17:27:09 +0800

    Generate dtbo

--- fpga2/build.sh
+++ fpga2/build.sh
@@ -12,3 +12,19 @@ quartus_sta "$n"
 quartus_asm "$n"
 quartus_cpf -c -o bitstream_compression=on "output_files/$n.sof" fpga.rbf
 
+sopc2dts \
+  --input top.sopcinfo \
+  --output fpga.dts \
+  --type dts \
+  --bridge-removal all \
+  --bridge-ranges bridge \
+  --overlay-target "/soc/base-fpga-region" \
+  --pov hps_0_bridges \
+  --pov-type overlay \
+  --no-timestamp \
+  --firmware-name fpga.rbf \
+  --verbose
+
+sed -i 's|/dts-v1/ /plugin/|/dts-v1/;\n/plugin/|' fpga.dts
+
+dtc --out-format dtb --out fpga.dtbo fpga.dts --symbols
