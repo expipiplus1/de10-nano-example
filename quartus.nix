619eb0f3fc76a4cf0aeab172a847754bce3f905b
Joe Hermaszewski <git@monoid.al>
2018-12-05 00:24:56 +0800

    Bump nixos-config for qsys fixes

--- quartus.nix
+++ quartus.nix
@@ -4,8 +4,8 @@ let
   nixos-config = pkgs.fetchFromGitHub {
     owner = "bjornfor";
     repo = "nixos-config";
-    rev = "784eb9445bd2c3ac96f363083e996d621f7c1478";
-    sha256 = "0q5wpd7yqg32gavhy4c7ka7pn3x672swxf9s7hkq3i4innr4cd5r";
+    rev = "ad9ccdcd027f3ee11306804f3e95e2ca1d756c8e";
+    sha256 = "0cs6725az0lgky7nbl7sdy5ydhrnv83kwkkhyb1vkf76zkygab42";
   };
   quartuss = pkgs.callPackage (nixos-config + "/pkgs/altera-quartus") {};
 in quartuss.altera-quartus-prime-standard-18
