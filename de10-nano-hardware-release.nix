5e89dd0a705144106e4b48bd3458851d890306fc
Joe Hermaszewski <git@monoid.al>
2018-12-05 23:44:36 +0800

    Remove unused nix files

--- de10-nano-hardware-release.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs }:
-
-pkgs.fetchzip {
-  url = "https://github.com/intel/de10-nano-hardware/releases/download/RELEASE-20180612_19.29.23/de10-nano-build_20180612.tgz";
-  sha256 = "1mmvjvc8vrfv1j681814nxlxn0d9hgil1id3n1bmri7gsinajjpd";
-  stripRoot = false;
-}
